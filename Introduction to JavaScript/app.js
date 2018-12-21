dff3e757e2e01454b956bf5d554f40e85fb6daed
haideralishah <headeralishah@gmail.com>
2018-12-21 17:43:26 +0500

    Chapter 1 to 5

---
+++ Introduction to JavaScript/app.js
@@ -0,0 +1,134 @@
+// Chapter 1
+
+
+// alert("loading JavaScript from External File");
+
+
+// alert("Hello JS");
+// alert( "Hello JS" );
+// // alert(" Hello JS ");
+// alert('Hello JS');
+
+// alert('"Hello JS"');
+// alert("'Hello JS'");
+
+
+// window.alert(' Hello JS ');
+
+
+// Hello JS
+//  Hello JS 
+// "Hello JS"
+// 'Hello JS'
+// " Hello JS "
+// 
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello to Console");
+// console.log(" Hello to Console ");
+// console.log("'Hello to Console'");
+// console.log(2 + 7);
+
+
+
+
+
+
+
+
+// Chapter 2
+
+
+// var username;
+// username = "Haider Ali";
+// console.log(username);
+
+
+// var rollnumber;
+// rollnumber = 2643;
+// console.log(rollnumber);
+
+
+
+// var userName = "haider";
+// console.log(userName);
+
+// userName = 'Ali';
+// console.log(userName);
+
+
+
+
+
+
+
+
+
+// var firstNumber = 2;
+// var secondNumber = 3;
+
+// var totalNumber = firstNumber + secondNumber;
+// //                   2        +    3
+
+// console.log(totalNumber);
+
+// console.log(secondNumber - firstNumber);
+
+
+
+
+
+
+
+// var firstName = 'Haider';
+// var lastName = 'Ali';
+
+// console.log(firstName + " " + lastName);
+
+// // Haider Ali
+// //$ _
+
+// var rose = 'flower';
+// var Rose = 'Something IDk'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var firstNumber = 9;
+// var secondNumber = 3;
+
+// var sumNumber = 3 % 9;
+
+// console.log(sumNumber);
+
+
+
+
+
+
+
+
+
+
+
+
