21dc19d45cd4156839d775ba8cc483a6f655dbf5
haideralishah <headeralishah@gmail.com>
2018-12-22 17:44:00 +0500

    Lab Assignment 1.

--- Introduction to JavaScript/app.js
+++ Introduction to JavaScript/app.js
@@ -6,7 +6,7 @@
 
 // alert("Hello JS");
 // alert( "Hello JS" );
-// // alert(" Hello JS ");
+// alert(" Hello JS ");
 // alert('Hello JS');
 
 // alert('"Hello JS"');
@@ -14,14 +14,15 @@
 
 
 // window.alert(' Hello JS ');
-
+// window.alert("'Hello JS'");
+// window.alert('" Hello JS "');
 
 // Hello JS
 //  Hello JS 
 // "Hello JS"
 // 'Hello JS'
 // " Hello JS "
-// 
+// 'hello JS
 
 
 
@@ -39,18 +40,24 @@
 // console.log(2 + 7);
 
 
+// console.log('Haider' + ' ' + 'Ali');
+// // Haider Ali
+// // Haider ali
 
 
-
+// console.log('1 + 1');
+// console.log(1 + 1);
+// 11
+// 2
 
 
 
 // Chapter 2
 
 
-// var username;
-// username = "Haider Ali";
-// console.log(username);
+// var userName;
+// userName = "Haider Ali";
+// console.log(userName);
 
 
 // var rollnumber;
@@ -117,7 +124,7 @@
 // var firstNumber = 9;
 // var secondNumber = 3;
 
-// var sumNumber = 3 % 9;
+// var sumNumber = 9 % 3;
 
 // console.log(sumNumber);
 
@@ -129,6 +136,112 @@
 
 
 
+// var initNumber = 1;
+
+// console.log(initNumber);
+
+// initNumber = initNumber + 1;
+// console.log(initNumber);
+
+
+// //           2 - 3   
+// initNumber = initNumber - 3;
+// console.log(initNumber);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// var initNumber = 5;
+
+// initNumber = initNumber * 3;
+
+// initNumber = initNumber + 1;
+
+// initNumber = initNumber / 4;
+
+// console.log(initNumber);
+
+
+
+
+// alert('haider \n ali');
+// console.log('haider \n'+ 'ali' );
+
+
+
+
+
+
+// var secondString = " in JavaScript.";
+
+// document.write("<b>Welcome</b> to DOM" + secondString + "<br />");
+
+// document.write(1 + 1);
+
+
+
+
+
+
+
+
+
+
+
+var username;
+
+
+var myName = "Haider Ali Shah";
+
+
+var message;
+message = "Hello World";
+alert(message);
+
+
+
+
+
+
+
+
+
+/*
+student name
+age
+institute name
+course name
+
+document.write();
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+var noOfChildren = 2;
+var wifeName = 'Alexa';
+var geoLocation = 'Canada';
+var jobTitle = 'IT Expert';
+
+alert("You will be an " + jobTitle + " in " + geoLocation + ", and married to " + wifeName + " with " + noOfChildren + "kids.");
