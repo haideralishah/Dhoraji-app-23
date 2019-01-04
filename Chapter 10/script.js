85bbf30cbeeda9015815838564dcd26ad4d50a79
haideralishah <headeralishah@gmail.com>
2019-01-04 18:03:33 +0500

    Class code for todays date.

---
+++ Chapter 10/script.js
@@ -0,0 +1,9 @@
+var a = prompt("enter a number ");
+if(a>5){
+  alert("the number is 5 or greater ..")
+}
+else{
+  alert("the number is less than 5")
+}
+
+
---
+++ JS Chapter 10-14/app.js
@@ -0,0 +1,286 @@
+
+
+
+
+// var userInput = +prompt('what is your age?');
+
+// if(userInput < 18){    
+//     console.log("Welcome to the Dhoraji Club.");
+// }
+// else{
+//     console.log("Sorry, you are not eligible.")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var num1 = 1;
+var num2 = 3;
+
+
+/*
+
+num1 === num2 // equals to
+num1 !== num2 // not equals to
+
+num1 > num2   // greater than
+num1 < num2   // smaller than
+
+num1 >= num2  // greater than or equals to
+num1 <= num2  // smaller than or equals to
+
+
+*/
+
+
+
+// var myAge = +prompt('Enter your age.');
+// var friendsAge = +prompt('Enter your friends age.');
+
+
+// if(myAge > friendsAge){
+//     console.log('I am older than my friend.');
+// }
+// else{
+//     console.log("My friend is older than me");
+// }
+
+
+
+
+
+
+
+
+
+
+
+// var shopName = prompt("Which shop is open?");
+
+// if(shopName === "Shamoon"){
+//     console.log('bring 1 KG Daal');
+// }
+// else if(shopName === "Mudassir"){
+//     console.log('bring 10 KG Daal');
+// }
+// else if(shopName === "Kamran"){
+//     console.log('bring half KG Daal');
+// }
+// else{
+//     console.log('bring cooked Daal 1 plate');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// and    &&
+// or     ||
+
+
+
+// var item1 = 'kurkure';
+// var item2 = 'pepsi';
+
+// if (item1 === 'lays' || item2 === 'pepsi') {
+//     console.log('bring pepsi and lays');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   80+   A+ 
+//   70+  80<   A  
+//   60+  70<   B
+//   50+  60<   C
+//   40+  50<   D
+//   33+  40<   E
+//   33<        F
+
+
+// var percentage = +prompt("what is your percentage?");
+
+
+// if(percentage >= 80){
+//     document.write('Congaratulations! You got A+');
+// }
+// else if(percentage >= 70 && percentage < 80){
+//     document.write('Congaratulations! You got A');
+// }
+// else if(percentage >= 60 && percentage < 70){
+//     document.write('You got B');
+// }
+// else if(percentage >= 50 && percentage < 60){
+//     document.write('You got C');
+// }
+// else if(percentage >= 40 && percentage < 50){
+//     document.write('You got D');
+// }
+// else if(percentage > 33 && percentage < 40){
+//     document.write('You got E');
+// }
+// else{
+//     document.write('Sorry! You are failed.');
+// }
+
+
+
+
+
+
+// var age = prompt('what is your age?');
+// var community = prompt('Which community you belong to?');
+
+// if(age <= 17 && community === 'dhoraji'){
+//     console.log('Congrats! you got admission.');
+// }
+
+// var community = prompt('which community you belong to?');
+// var grade = prompt("what grade you obtained?");
+
+// if(community === 'dhoraji' || grade === 'A+'){
+//     console.log('Congrats! you got admission.');
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var age = prompt('what is your age?');
+// var format = prompt('which format you want to play?');
+// var team;
+// if (age >= 18) {
+//     if (format === 'test') {
+//         console.log('you qualified for adult test cricket team');
+//     }
+//     else if (format === 'od') {
+//         console.log('You qualified for adult one day team.');
+//     }
+//     else {
+//         console.log('you qualified for adult T20 team.');
+//     }
+
+// }
+// else {
+//     if(format === 'test'){
+//         console.log('you qualified for under 18 test team');
+//     }
+//     else if(format === 'od'){
+//         console.log('You qualified for under 18 one day team.');
+//     }
+//     else{
+//         console.log('you qualified for under 18 T20 team.');
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
