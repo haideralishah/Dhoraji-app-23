b899f2a75926646cadd80fca09d7663d6d662097
haideralishah <headeralishah@gmail.com>
2019-02-15 18:04:34 +0500

    class revision code for JS test 2

---
+++ JS Chapter 39 to 42/app.js
@@ -0,0 +1,150 @@
+
+// var dayOfWk = '';
+
+
+// if (dayOfWk === "Sat" || dayOfWk === "Sun") {
+//     alert("Whoopee!");
+// }
+// else if (dayOfWk === "Fri") {
+//     alert("TGIF!");
+// }
+// else {
+//     alert("Shoot me now!");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var dayOfWk = 'mon'
+
+
+
+// switch (dayOfWk) {
+//     case 'sat':
+//         alert('it is a holiday');
+//         // break;
+//     case 'sun':
+//         alert('it is a fun day');
+//         break;    
+//     default:
+//         alert('it is a working day');
+// }
+
+
+
+
+// switch (true) {
+//     case age>18:
+//         alert('old enough');
+//         break;
+     
+//     default:
+//         alert('young enough');
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (var i = 0; i < 10; i++) {
+
+// }
+
+// var i = 0;
+// while (i < 20) {
+//     console.log(i);
+//     if (i % 2 === 0) {
+//         i++;
+//     }
+//     else {
+//         i = i + 3;
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var i = 0;
+// while (i <= 3) {
+//     alert(i);
+//     i++;
+// }
+
+
+
+
+// var i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JS Revision for JS Test 2/app.js
@@ -0,0 +1,88 @@
+
+
+
+
+
+
+// function doSum() {
+//     var sum = num1 + num2;
+//     alert(sum);
+// }
+
+
+// doSum();
+// doSum();
+// doSum();
+// doSum();
+// doSum();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function bringNashta(){
+
+//     console.log('bring me a cup of tea with some snacks');
+// }
+
+
+// bringNashta();
+
+
+// bringNashta();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function doSum(num2, num1) {
+//     var sum = num1 + num2;
+//     console.log(sum);
+// }
+
+
+// doSum(2, 4); // 6
+// doSum(4, 8); // 12 
+// doSum(7, 1); // 8
+
+
+
+
+
+
+
+
+function doSum(num2, num1) {
+    var sum = num1 + num2;
+    return sum;
+}
+
+
+var mySum = doSum(2, 7);
+
+console.log(mySum);
