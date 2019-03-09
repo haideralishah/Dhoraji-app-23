948fb95b32df28735fb38d7c9fac3af6d61e1acb
haideralishah <headeralishah@gmail.com>
2019-03-09 19:52:59 +0500

    class code for DOM

---
+++ JS Chapter 58 to/app.js
@@ -0,0 +1,52 @@
+
+
+// var elem = document.childNodes[1]
+//                 .childNodes[2]
+//                 .childNodes[1];
+// console.log(elem);
+// elem.innerHTML = 'Hello JavaScript';
+
+
+
+
+
+
+
+
+// function add() {
+
+//     var elem = document.childNodes[1]
+//         .childNodes[2]
+//         .childNodes;
+//     console.log(elem[3]);
+//     console.log(elem[7]);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var bigCity = document.getElementById('citiesname');
+console.log(bigCity.childNodes[5].innerHTML);
+
+
+
+
+
+
+
+
+
+
+
